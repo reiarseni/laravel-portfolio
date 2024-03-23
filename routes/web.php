55f28c6786c643ca562f4d98751eeced90dd5a96
reiarseni <reiarseni@gmail.com>
2024-03-22 22:39:54 -0600

    Add Skill and Project controllers, models, migrations and routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Backend\ProjectController;
+use App\Http\Controllers\Backend\SkillController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth'])->group(function() {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::resource('/skills', SkillController::class);
+    Route::resource('/projects', ProjectController::class);
+});
+
+require __DIR__.'/auth.php';
