63a52bf0b378b4740d30d1d3823bfd5764f2e2d2
reiarseni <reiarseni@gmail.com>
2024-03-23 13:41:10 -0600

    Add new button in skills and project lists

--- resources/views/skills/index.blade.php
+++ resources/views/skills/index.blade.php
@@ -1,12 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Projects') }}
+            {{ __('Skills') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex justify-end m-2 p-2">
+                <a href="{{ route('skills.create')}}"
+                    class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">
+                    New Skill
+                </a>
+            </div>
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
 
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
